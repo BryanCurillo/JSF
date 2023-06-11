3d0be5610bf24eed1abb0d72d91091e36cf7291d
BryanCurillo <102774552+BryanCurillo@users.noreply.github.com>
2023-06-10 19:17:17 -0500

    Add files via upload

---
+++ jsp_curillo/build/generated-sources/ap-source-output/modelo/Empleado_.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-06-10T18:38:29")
+@StaticMetamodel(Empleado.class)
+public class Empleado_ { 
+
+    public static volatile SingularAttribute<Empleado, Integer> idDep;
+    public static volatile SingularAttribute<Empleado, String> apellido;
+    public static volatile SingularAttribute<Empleado, Long> idEmp;
+    public static volatile SingularAttribute<Empleado, String> nombre;
+
+}
